c2056fb0a460b368d261e04bbbd448c6652a8189
masseaxe <masse.axel@gmail.com>
2018-02-16 12:29:26 +0100

    graphic header - dashboard

--- resources/views/backoff/header.blade.php
+++ resources/views/backoff/header.blade.php
@@ -9,19 +9,32 @@
                 <span class="icon-menu"></span>
             </button>
             <ul class="navbar-nav">
-                <li class="nav-item dropdown d-none d-lg-flex">
+                <li class="nav-item d-none d-lg-flex">
                     <a class="nav-link  nav-btn"  href="#" >
                         <span class="btn">+ Campagne</span>
                     </a>
                 </li>
+                <li class=" nav-item  d-flex align-items-center justify-content-md-center">
+                    <i class="icon-bubbles icon-sm"></i>
+                    <div class="ml-3 text-center" style="line-height: 13px;">
+                        <small class="mb-0">
+                            <u>PROCHAIN CMM</u><br>
+                            12/02 à 10H30 (Maurice Leblanc)<br>
+                            Animé par le Nicolas & Philippine</small>
+                    </div>
+                </li>
             </ul>
             <ul class="navbar-nav navbar-nav-right">
+                <li class="nav-item btn-group ml-auto mr-2 border-0">
+                    <input type="text" class="form-control " placeholder="Rechercher" />
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
                         <i class="icon-arrow-down mx-0"></i>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-                        <a class="dropdown-item preview-item">
+                        <a class="dropdown-item preview-item" href="#">
+
                             <div class="preview-thumbnail">
                                 <div class="preview-icon">
                                     <i class="icon-settings mx-0"></i>
@@ -32,7 +45,7 @@
                             </div>
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item preview-item">
+                        <a class="dropdown-item preview-item" href="#">
                             <div class="preview-thumbnail">
                                 <div class="preview-icon">
                                     <i class="icon-logout mx-0"></i>
@@ -44,7 +57,6 @@
                         </a>
                     </div>
                 </li>
-
             </ul>
             <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
                 <span class="icon-menu"></span>
