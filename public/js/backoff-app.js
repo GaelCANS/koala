e15ea26fb80f71155bce55f83549d8b26c41fc50
gaelCANS <gael.levant@ca-normandie-seine.fr>
2018-02-19 11:58:48 +0100

    init select2 js

--- public/js/backoff-app.js
+++ public/js/backoff-app.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
 
+    /**
+     * Gestion des dates pour éviter qu'elles apparaissent à 0000-00-00 dans les champs input
+     */
     $('.date-not-null').each(function(){
         if ($(this).val() == '0000-00-00')
             $(this).val('');
     });
+
+
+
+    /**
+     * Initialisation du select2
+     */
+    $('select[multiple]').select2();
+    $('select.select2').select2();
+
+    
 
 });
