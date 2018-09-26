30071f6be8fbdc7487d79babbe13b1dc216a3c21
Gael <gael.levant@ca-normandie-seine.fr>
2018-09-26 14:47:23 +0200

    order on channel and remove goal

--- resources/views/campaignchannels/channelIndicators.blade.php
+++ resources/views/campaignchannels/channelIndicators.blade.php
@@ -4,11 +4,11 @@
         {!! Form::hidden( 'indicator['.$campaignChannelIndicator->uniqid.'][uniqid]' , $campaignChannelIndicator->uniqid , array( 'class' => 'form-control bg-light text-center mb-0' ) ) !!}
         {!! Form::hidden( 'indicator['.$campaignChannelIndicator->uniqid.'][campaign_channel_id]' , $channel->id , array( 'class' => 'form-control bg-light text-center mb-0' ) ) !!}
     </li>
-    <li class="list-inline-item">
+    <!--<li class="list-inline-item">
         <div class="form-group mb-0">
             {!! Form::number( 'indicator['.$campaignChannelIndicator->uniqid.'][goal]' , $campaignChannelIndicator->goal , array( 'class' => 'form-control duplicatable-indicator bg-light text-muted mb-0 p-1' , 'data-name' => 'goal' , 'data-indicator' => $campaignChannelIndicator->Indicator->id ) ) !!}
         </div>
-    </li>
+    </li>-->
     <li class="list-inline-item">
         <div class="form-group mb-0">
             {!! Form::number( 'indicator['.$campaignChannelIndicator->uniqid.'][result]' , $campaignChannelIndicator->result , array( 'class' => 'form-control duplicatable-indicator bg-light mb-0 p-1' , 'data-name' => 'result' , 'data-indicator' => $campaignChannelIndicator->Indicator->id ) ) !!}
