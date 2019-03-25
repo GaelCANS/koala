aaddb16842fc5a9b15825f5364668243588bb53b
Gael <gael.levant@ca-normandie-seine.fr>
2019-03-25 15:43:35 +0100

    title 404

--- resources/views/campaigns/campaignchannelindicators.blade.php
+++ resources/views/campaigns/campaignchannelindicators.blade.php
@@ -4,7 +4,7 @@
         <label class="label-indicator">{{$indicator->name}}</label>
         {!! Form::hidden( 'indicator['.$cci->uniqid.'][uniqid]' , $cci->uniqid , array( 'class' => 'form-control bg-light text-center mb-0' ) ) !!}
         {!! Form::hidden( 'indicator['.$cci->uniqid.'][campaign_channel_id]' , $channel->pivot->id , array( 'class' => 'form-control bg-light text-center mb-0' ) ) !!}</li>
-        </div>
+        </li>
     <!--<li class="list-inline-item">
         <div class="form-group mb-0">
             {!! Form::number( 'indicator['.$cci->uniqid.'][goal]' , $cci->goal , array( 'class' => 'form-control duplicatable bg-light text-muted mb-0 p-1' , 'data-name' => 'goal' ) ) !!}
