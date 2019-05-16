c82f1c2f3890b8d65861c8f5fd34a639629b9032
Gael <gael.levant@ca-normandie-seine.fr>
2019-05-16 14:31:38 +0200

    middleware auth on stats

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\DB;
 
 class StatisticController extends Controller
 {
+
+    public function __construct( ){
+
+        $this -> middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
