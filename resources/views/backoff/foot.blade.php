d03b73e83b759715c53f0efd79f50d8cfa4cabca
Gael <gael.levant@ca-normandie-seine.fr>
2019-05-22 11:18:19 +0200

    count increment

--- resources/views/backoff/foot.blade.php
+++ resources/views/backoff/foot.blade.php
@@ -482,6 +482,20 @@
             $('#stat-content').html(data.html);
         });
     }
+    
+    
+    $('.increment-animate').each(function () {
+
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 1000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    })
 
 
 
--- resources/views/statistics/increment.blade.php
+++ resources/views/statistics/increment.blade.php
@@ -1,4 +1,4 @@
 <div class="indicator-channel">
-    <h2 style="color:#fb9678;" class="{{$channel->class_name}}">{{round($indicator['average'])}}</h2>
+    <h2 style="color:#fb9678;" class="{{$channel->class_name}} increment-animate">{{round($indicator['average'])}}</h2>
     <span class="indicator-subtitle text-muted mt-2">@if ($indicator['id'] != \App\Parameter::getParameter('best_banniere' , 'dashboard') ) {{$indicator['indicator']}} @else Clics/jour @endif</span>
 </div>
