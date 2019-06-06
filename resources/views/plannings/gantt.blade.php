f715060236fc9d9cf9816a0156ed78c30abc3f60
Gael <gael.levant@ca-normandie-seine.fr>
2019-06-06 11:42:20 +0200

    fix on planning-timeline

--- resources/views/plannings/gantt.blade.php
+++ resources/views/plannings/gantt.blade.php
@@ -1,18 +1,17 @@
-
 <div class="row coucou" style="margin-top: 12px">
     <div class="col-md-12">
         <div
-            id="gantt-camp" data-begin="{{!empty($request->begin) ? $request->begin : ''}}" data-end="{{!empty($request->end) ? $request->end : ''}}"
-            data-min="{{\Carbon\Carbon::createFromFormat('d/m/Y',$request->begin)->format('Y-m-d')}}"
-            data-max="{{\Carbon\Carbon::createFromFormat('d/m/Y',$request->end)->format('Y-m-d')}}"
-            >
+                id="gantt-camp" data-begin="{{!empty($request->begin) ? $request->begin : ''}}" data-end="{{!empty($request->end) ? $request->end : ''}}"
+                data-min="{{\Carbon\Carbon::createFromFormat('d/m/Y',$request->begin)->format('Y-m-d')}}"
+                data-max="{{\Carbon\Carbon::createFromFormat('d/m/Y',$request->end)->format('Y-m-d')}}"
+        >
         </div>
     </div>
 </div>
 
 
 @forelse($campaigns as $campaignChannel)
-    @if ($campaignChannel->begin != '0000-00-00' && $campaignChannel->end != '0000-00-00')
+    @if ($campaignChannel->begin != '0000-00-00' && $campaignChannel->end != '0000-00-00' && isset($campaignChannel->Campaign))
         <div
                 class="timeline-channel"
                 data-channel="{{$campaignChannel->Campaign->name}}"
