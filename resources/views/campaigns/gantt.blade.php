1873aa2fe2da9fa1a37842fd88af9a20422a402b
Gael <gael.levant@ca-normandie-seine.fr>
2019-06-11 14:03:40 +0200

    graphic updates on campaign timeline

--- resources/views/campaigns/gantt.blade.php
+++ resources/views/campaigns/gantt.blade.php
@@ -13,24 +13,30 @@
             </div>
         </div>
     </div>
-    <div class="row" style="margin-top: 12px">
-        <div class="col-md-12">
-            <div
-                id="gantt-camp"
-                style="height: 550px;"
-                data-min="{{\Carbon\Carbon::createFromFormat('d/m/y',$campaign->begin)->subMonth()->format('Y-m-d')}}"
-                data-max="{{\Carbon\Carbon::createFromFormat('d/m/y',$campaign->end)->addMonth()->format('Y-m-d')}}"
-            ></div>
+
+
+    <div class="card grid-margin">
+        <div class="card-body">
+            <div class="row" style="margin-top: 12px">
+                <div class="col-md-12">
+                    <div
+                            id="gantt-camp"
+                            style="overflow:hidden;"
+                            data-min="{{\Carbon\Carbon::createFromFormat('d/m/y',$campaign->begin)->subMonth()->format('Y-m-d')}}"
+                            data-max="{{\Carbon\Carbon::createFromFormat('d/m/y',$campaign->end)->addMonth()->format('Y-m-d')}}"
+                    ></div>
+                </div>
+            </div>
         </div>
     </div>
     @forelse($campaign->relationCampaignChannels as $campaignChannel)
         @if ($campaignChannel->begin != '0000-00-00' && $campaignChannel->end != '0000-00-00')
             <div
-                class="timeline-channel"
-                data-channel="{{$campaignChannel->Channel->name}}"
-                data-family="{{$campaignChannel->Channel->class_name}}"
-                data-begin="{{$campaignChannel->begin}}"
-                data-end="{{$campaignChannel->end}}"
+                    class="timeline-channel"
+                    data-channel="{{$campaignChannel->Channel->name}}"
+                    data-family="{{$campaignChannel->Channel->class_name}}"
+                    data-begin="{{$campaignChannel->begin}}"
+                    data-end="{{$campaignChannel->end}}"
             >
             </div>
         @endif
